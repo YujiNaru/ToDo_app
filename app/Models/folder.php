f450b861fded6311c98e8f68ca4bea10027d766b
YujiNaru <y_narukawa@argo-aichi.com>
2023-01-15 17:10:50 +0900

    third commit

--- app/Models/folder.php
+++ app/Models/folder.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class folder extends Model
+class Folder extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+
+    public function tasks()
+    {
+        return $this->hasMany('App\Models\Task');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 */
-
+Auth::routes();
+Route::get('/', 'App\Http\HomeController@index')->name('home');
 Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
+Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
+Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
+Route::get('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', 'App\Http\Controllers\TaskController@edit');
